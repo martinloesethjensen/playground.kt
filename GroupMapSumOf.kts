87cef4ecf95f64afe7e76f74f60dee8c662fdf77
Martin L. Jensen <mlje@trifork.com>
2023-04-20 10:57:57 +0200

    group data by month and code with the sum amount

--- GroupMapSumOf.kts
+++ GroupMapSumOf.kts
@@ -1,27 +1,49 @@
-data class Data(val code: String, val amount: Double)
+import java.time.Month
+
+data class Data(val month: Month, val code: String, val amount: Double)
 
 val dataList = listOf(
-    Data(code = "AAA", amount = 11.0),
-    Data(code = "AAA", amount = 50.0),
-    Data(code = "BBB", amount = 3.0),
-    Data(code = "CCC", amount = 100.0),
-    Data(code = "CCC", amount = 25.0),
+    Data(month = Month.JANUARY, code = "AAA", amount = 11.0),
+    Data(month = Month.FEBRUARY, code = "AAA", amount = 50.0),
+    Data(month = Month.MARCH, code = "BBB", amount = 3.0),
+    Data(month = Month.MARCH, code = "CCC", amount = 100.0),
+    Data(month = Month.APRIL, code = "CCC", amount = 25.0),
+    Data(month = Month.APRIL, code = "CCC", amount = 25.0),
+    Data(month = Month.APRIL, code = "DDD", amount = 300.0),
 )
+
+val groupedDataByMonthWithSum = dataList
+    .groupBy { it.month }
+    .mapValues { monthlyData ->
+        monthlyData.value
+            .groupBy { it.code }
+            .mapValues {
+                it.value.sumOf { data ->
+                    data.amount
+                }
+            }
+    }
+
+println(groupedDataByMonthWithSum)
+
+val maxByMonth = groupedDataByMonthWithSum.values.flatMap { it.values }.max()
+
+println(maxByMonth)
 
 val groupedDataWithSum = dataList
     .groupBy { it.code }
     .mapValues {
-        it.value.sumOf { species ->
-            species.amount
+        it.value.sumOf { data ->
+            data.amount
         }
     }
 
-println(groupedDataWithSum.toString())
+println(groupedDataWithSum)
 
 val max = groupedDataWithSum.maxBy { it.value }
 
-println(max.toString())
+println(max)
 
 val sortedDataByValue = groupedDataWithSum.entries.sortedBy { it.value }.asReversed()
 
-println(sortedDataByValue.toString())
+println(sortedDataByValue)
